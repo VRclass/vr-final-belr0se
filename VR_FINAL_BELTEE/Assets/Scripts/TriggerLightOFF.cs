bc14505324ed44dc06022bf56617c334a5c15a79
Bel <bunbbi@Bels-MacBook-Pro.local>
2019-04-29 16:19:16 -0500

    Code Swaps
    asdasdasdads

---
+++ VR_FINAL_BELTEE/Assets/Scripts/TriggerLightOFF.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLightOFF : MonoBehaviour
+{
+    public Light lightToSwitch = null;
+
+    // When the trigger is entered 
+    public void OnTriggerEnter(Collider other)
+    {
+        lightToSwitch.enabled = false; // turn the light off
+        print("entered the OFF trigger");  // print the message
+    }
+
+}
---
+++ VR_FINAL_BELTEE/Assets/Scripts/TriggerLightON.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLightON : MonoBehaviour
+{
+    public Light lightToSwitch = null;
+
+    // When the trigger is entered 
+    public void OnTriggerEnter(Collider other)
+    {
+        lightToSwitch.enabled = true; // turn the light off
+        print("entered the ON trigger");  // print the message
+    }
+
+}
